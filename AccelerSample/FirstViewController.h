0f04aa9ca7842cbb86fef0661a4742e5feb858a2
Takanori727 <ysn_tknr@yahoo.co.jp>
2014-03-18 09:27:34 +0800

    Initial Commit

---
+++ AccelerSample/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  AccelerSample
+//
+//  Created by 孝則 吉野 on 2014/03/18.
+//  Copyright (c) 2014年 孝則 吉野. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ AccelerSample/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  AccelerSample
+//
+//  Created by 孝則 吉野 on 2014/03/18.
+//  Copyright (c) 2014年 孝則 吉野. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
